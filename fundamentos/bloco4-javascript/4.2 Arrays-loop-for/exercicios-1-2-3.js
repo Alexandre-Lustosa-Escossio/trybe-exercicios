0e95fd8d626c4886b2d9599e1ee4e488efda1e27
Alexandre <lutosaalexandre@gmail.com>
2021-11-08 15:28:01 -0300

    exercicios pre-aula bloco 4.2

---
+++ fundamentos/bloco4-javascript/4.2 Arrays-loop-for/exercicios-1-2-3.js
@@ -0,0 +1,21 @@
+//Exercicio 1
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+console.log(menu.indexOf('Portfólio'))
+
+menu.push('Contato')
+console.log(menu + '\n')
+
+//Exercicio 2
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for(let index = 0;index < groceryList.length;index += 1){
+    console.log(groceryList[index] + '\n')
+}
+
+//Exercicio 3
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for(let name of names){
+    console.log(name + '\n')
+}
